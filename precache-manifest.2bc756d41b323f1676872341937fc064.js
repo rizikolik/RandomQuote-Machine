9e9564253ac6d347c2fd6b349eb67f4bf0d8d561
rizikolik <ynsture@gmail.com>
2019-07-29 00:33:23 +0300

    Updates

---
+++ precache-manifest.2bc756d41b323f1676872341937fc064.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5ccae9b8ab12ccce023dd61374b5605",
+    "url": "/RandomQuote-Machine/index.html"
+  },
+  {
+    "revision": "2762a13fc1bdad37e9e8",
+    "url": "/RandomQuote-Machine/static/css/main.bc1b6fcf.chunk.css"
+  },
+  {
+    "revision": "613c2f8fedf96580e0e2",
+    "url": "/RandomQuote-Machine/static/js/2.e1bd6bf8.chunk.js"
+  },
+  {
+    "revision": "2762a13fc1bdad37e9e8",
+    "url": "/RandomQuote-Machine/static/js/main.f109d40a.chunk.js"
+  },
+  {
+    "revision": "a7bf908c7416baa5eb79",
+    "url": "/RandomQuote-Machine/static/js/runtime~main.214e46bf.js"
+  }
+]);
